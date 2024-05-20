90acefd6bdd8101aade43a10f7f6da0c2a49205f
alexsandroferreira <ferreira.alex@outlook.com>
2024-05-20 09:57:55 -0300

    feat: add table component

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -8,6 +8,7 @@ import {
 } from 'lucide-react'
 
 import { IconButton } from './icon-button'
+import { Table } from './table/table'
 export function AttendeeList() {
   return (
     <div className="flex flex-col gap-4">
@@ -26,113 +27,111 @@ export function AttendeeList() {
           />
         </div>
       </div>
-      <div className="border border-white/10 rounded-lg">
-        <table className="w-full">
-          <thead>
-            <tr className="border-b border-white/10">
-              <th
-                style={{ width: 48 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              >
-                <input
-                  type="checkbox"
-                  className="size-4 bg-black/20 rounded border border-white/10"
-                  name=""
-                  id=""
-                />
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Código
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Participantes
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data de inscrição
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data do check-in
-              </th>
-              <th
-                style={{ width: 64 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              ></th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 10 }).map((_, index) => {
-              return (
-                <tr
-                  key={index}
-                  className="
+      <Table>
+        <thead>
+          <tr className="border-b border-white/10">
+            <th
+              style={{ width: 48 }}
+              className="py-3 px-4 text-sm font-semibold text-left"
+            >
+              <input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border border-white/10"
+                name=""
+                id=""
+              />
+            </th>
+            <th className="py-3 px-4 text-sm font-semibold text-left">
+              Código
+            </th>
+            <th className="py-3 px-4 text-sm font-semibold text-left">
+              Participantes
+            </th>
+            <th className="py-3 px-4 text-sm font-semibold text-left">
+              Data de inscrição
+            </th>
+            <th className="py-3 px-4 text-sm font-semibold text-left">
+              Data do check-in
+            </th>
+            <th
+              style={{ width: 64 }}
+              className="py-3 px-4 text-sm font-semibold text-left"
+            ></th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: 10 }).map((_, index) => {
+            return (
+              <tr
+                key={index}
+                className="
             border-b 
             border-white/10
             hover:bg-white/5"
-                >
-                  <td className="py-3 px-4 text-sm text-zinc-300">
-                    <input
-                      type="checkbox"
-                      className="size-4 bg-black/20 rounded border border-white/10"
-                      name=""
-                      id=""
-                    />
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300">232323</td>
-                  <td className="py-3 px-4 text-sm text-zinc-300">
-                    <div className="flex flex-col gap-1">
-                      <span className="font-semibold text-white">
-                        Don Diego de la Vaga
-                      </span>
-                      <span>donDiegoVega@gmail.com</span>
-                    </div>
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300">
-                    8 dias atrás
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300">
-                    5 dias atrás
-                  </td>
-                  <td className="py-3 px-4 text-sm text-zinc-300">
-                    <IconButton transparent>
-                      <MoreHorizontal className="size-4" />
-                    </IconButton>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-                Mostrando 10 de 120 itens
-              </td>
-              <td
-                className="py-3 px-4 text-sm text-zinc-300 text-right"
-                colSpan={3}
               >
-                <div className="inline-flex items-center gap-8">
-                  <span> Página 1 de 22</span>
-                  <div className="flex gap-1">
-                    <IconButton>
-                      <ChevronsLeft className="size-4" />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronLeft className="size-4" />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronRight className="size-4" />
-                    </IconButton>
-                    <IconButton>
-                      <ChevronsRight className="size-4" />
-                    </IconButton>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border border-white/10"
+                    name=""
+                    id=""
+                  />
+                </td>
+                <td className="py-3 px-4 text-sm text-zinc-300">232323</td>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">
+                      Don Diego de la Vaga
+                    </span>
+                    <span>donDiegoVega@gmail.com</span>
                   </div>
+                </td>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  8 dias atrás
+                </td>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  5 dias atrás
+                </td>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  <IconButton transparent>
+                    <MoreHorizontal className="size-4" />
+                  </IconButton>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+              Mostrando 10 de 120 itens
+            </td>
+            <td
+              className="py-3 px-4 text-sm text-zinc-300 text-right"
+              colSpan={3}
+            >
+              <div className="inline-flex items-center gap-8">
+                <span> Página 1 de 22</span>
+                <div className="flex gap-1">
+                  <IconButton>
+                    <ChevronsLeft className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronLeft className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+                  <IconButton>
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+              </div>
+            </td>
+          </tr>
+        </tfoot>
+      </Table>
     </div>
   )
 }
